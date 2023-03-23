9585bbb6fa6e75dd043221e5ce468bfeb187d43d
Daniel Duhon <danielgduhon@gmail.com>
2023-03-23 09:25:13 -0500

    Added .gitignore

--- userData.py
+++ userData.py
@@ -5,11 +5,13 @@ import requests
 import os
 import dotenv
 
-VATSIM_AUTH = os.getenv('VATSIM_AUTH_ENDPOINT') + '/oauth/token'
+# VATSIM_AUTH = os.getenv('VATSIM_AUTH_ENDPOINT') + '/oauth/token'
+USER_DATA_URL = 'https://api.zfwartcc.net/user?id='
 
-VATSIM_CLIENT_ID = os.getenv('VATSIM_AUTH_CLIENT_ID')
-VATSIM_CLIENT_SECRET = os.getenv('VATSIM_AUTH_CLIENT_SECRET')
-userData = 'https://api.zfwartcc.net/user?id='
+# def getAuthToken():
+#     CLIENT_ID = os.getenv('VATSIM_AUTH_CLIENT_ID')
+#     CLIENT_SECRET = os.getenv('VATSIM_AUTH_CLIENT_SECRET')
+#     TOKEN_URL = os.getenv('VATSIM_AUTH_ENDPOINT') + '/oauth/token'
 
 # oauth2client = OAuth2Client(
 #     tokenEndpoint = VATSIM_AUTH,
@@ -18,12 +20,12 @@ userData = 'https://api.zfwartcc.net/user?id='
 
 # Fetch user data from server
 def getUserData(cid):
-    rawData = requests.get(userData + str(cid))
+    rawData = requests.get(USER_DATA_URL + str(cid))
     jsonData = json.loads(rawData.text)
 
     return jsonData
 
 
 ddTest = getUserData(1223849)
-print(userData + str(1223849))
+print(USER_DATA_URL + str(1223849))
 print(ddTest)
